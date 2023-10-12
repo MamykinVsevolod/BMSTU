6268988397b3260c0fb7ee69f74c5544f8026f5b
MamykinVsevolod <vsevolodmamykin@gmail.com>
2023-10-12 08:34:35 +0300

    готовая 5 лаба

--- 3semestr/YAIP/lab5/3/functions.rb
+++ 3semestr/YAIP/lab5/3/functions.rb
@@ -1,9 +1,37 @@
 # frozen_string_literal: true
 
-def permutations(my_array)
+def permutations(my_arr)
+  my_array = my_arr.clone
+
   my_array.each_index do |i|
     my_array[i], my_array[i + 1] = my_array[i + 1], my_array[i] unless i.odd?
   end
 
   my_array.join(' ')
+end
+
+def proverka(my_arr)
+  flag = true
+
+  my_array = my_arr.clone
+
+  my_array2 = my_arr.clone
+
+  my_array2.each_index do |i|
+    my_array2[i], my_array2[i + 1] = my_array2[i + 1], my_array2[i] unless i.odd?
+  end
+
+  my_array2.each_index do |i|
+    flag = false if !i.odd? && (my_array2[i] != my_array[i - 1])
+  end
+
+  if flag
+
+    my_array.join(' ')
+
+  else
+
+    my_array2.join(' ')
+
+  end
 end
--- 3semestr/YAIP/lab5/3/tests.rb
+++ 3semestr/YAIP/lab5/3/tests.rb
@@ -1,9 +1,58 @@
 # frozen_string_literal: true
 
+
+
 require 'minitest/autorun'
+
+
 
 require_relative '1'
 
+
+
 class TestFunc < Minitest::Test
-  def test_func1; end
+
+  def test_func1
+
+    s = ''
+
+
+
+    1.upto(rand(1..10) * 2) { s += (1..rand(30)).map { rand(97..122).chr }.join + ' ' }
+
+
+
+    s = s.delete_suffix(' ')
+
+
+
+    s += '.'
+
+
+
+    my_arr = s.split
+
+
+
+    my_arr2 = s.split
+
+
+
+    my_arr[-1] = my_arr[-1].delete_suffix('.')
+
+
+
+    my_arr2[-1] = my_arr2[-1].delete_suffix('.')
+
+
+
+    print 'test: ' + s
+
+
+
+    assert_equal(proverka(my_arr), permutations(my_arr2))
+
+  end
+
 end
+
