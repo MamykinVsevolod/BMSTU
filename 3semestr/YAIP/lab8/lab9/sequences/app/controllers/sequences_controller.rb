5cf4c2bd1aaab9fd66423db0452f05ee2e6bfae5
MamykinVsevolod <vsevolodmamykin@gmail.com>
2023-11-19 15:18:45 +0300

    готовые тесты для 8 лабы

--- 3semestr/YAIP/lab8/lab9/sequences/app/controllers/sequences_controller.rb
+++ 3semestr/YAIP/lab8/lab9/sequences/app/controllers/sequences_controller.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
 class SequencesController < ApplicationController
-  def input
-  end
+  def input; end
+
   def view
     # Функция, которая проверяет, является ли подпоследовательность полными квадратами
     longest_subsequence = []  # Самая длинная подпоследовательность
     current_subsequence = []  # Текущая подпоследовательность
     all_subsequences = [] # Все подпоследовательности
-    sequence = params[:v2].split(' ').map(&:to_i)
-    sequence.each do |number|
+    # unless params[:v2].nil?
+    sequence = params[:v2]&.split(' ')&.map(&:to_i)
+    sequence&.each do |number|
       if (Math.sqrt(number) % 1).zero?
         # Если число является полным квадратом
         current_subsequence << number
@@ -27,8 +28,11 @@ class SequencesController < ApplicationController
     longest_subsequence = current_subsequence.clone if current_subsequence.length > longest_subsequence.length
     all_subsequences.pop if all_subsequences[all_subsequences.size - 1] == ''
     subsequence_count = longest_subsequence.length
-    @number = [params[:v1], sequence.size.to_s]
-    @result = [all_subsequences, longest_subsequence.join(' '), subsequence_count.to_s, sequence.join(' ')]
-
+    @number = [params[:v1], sequence&.size.to_s]
+    @result = [all_subsequences, longest_subsequence.join(' '), subsequence_count.to_s, sequence&.join(' ')]
+    # end
+    # @number = [params[:v1], 0]
+    # @result = [all_subsequences, '', '0', '']
+    logger.debug "from view controller: #{@result}"
   end
 end
